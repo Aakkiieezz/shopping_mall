844ea899319fc489950c88e03c6a5f4b529db55c
Akash Kadole <kadoleakash96@gmail.com>
2022-01-05 14:23:38 +0530

    Delete Backend directory

--- Backend/sql_connection.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import mysql.connector
-__cnx = None
-
-def get_sql_connection():
-  global __cnx
-  if __cnx is None:
-    __cnx = mysql.connector.connect(user='root', password='Aakkiieezz21', database='ShoppingMall')
-  return __cnx
